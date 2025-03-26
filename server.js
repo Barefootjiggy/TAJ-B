5da92208c7310c0bf7321775bb191100863553b5
Josh <jpgiant@yahoo.com>
2025-03-26 19:05:53 -0400

    Allow CORS from Netlify

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "http://localhost:3000", 
+  origin: ["http://localhost:3000", "https://trainwithamandajane.com"],
   methods: ["GET", "POST"],
   credentials: true,
 }));
